adaf394a0505e08236baa95fd3f491b004f12936
Instrutor Grau Tecnico <INSTRUTOR@graubh.com.br>
2021-07-15 20:53:04 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          creat-react-app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    autores: []
+  }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts/')
+      .then(res => {
+        const autores = res.data;
+        this.setState({ autores })
+      })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Container className="p-3">
+            <h1>Lista de Posts</h1>
+            <br />
+            <ListGroup as="ul">
+              {this.state.autores.map(autor =>
+                <div style={{ textAlign: "left" }} key={autor.id}>
+                  <ListGroup.Item as="li" active>
+                    {autor.title}
+                  </ListGroup.Item>
+
+                  <ListGroup.Item as="li">
+                    {autor.body}
+                  </ListGroup.Item>
+                  <br />
+                </div>
+              )}
+            </ListGroup>
+          </Container>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
